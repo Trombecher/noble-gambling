fa0f0da2b2f7baf2686438e6c9d7a34817feebc0
Niklas Post <Niklas.Post@schule.local>
2024-02-23 12:50:06 +0100

    red bg

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,28 @@ export default function App() {
     const url = new URL(location.href);
     const balance = new Box(+(url.searchParams.get("balance") || 0));
 
-    addListenerRecursively({balance}, () => {
+    addListenerRecursively({ balance }, () => {
         url.searchParams.set("balance", String(balance.value));
         history.pushState(null, "", url);
     });
 
     return (
         <>
-            <header class={"flex p-4"}>
+            <header class={"flex p-4 w-full"}>
                 <h1 class={"mr-auto font-fancy text-shade-50"}>Noble Gambling</h1>
                 <div>${insertBoxAsText(balance)}</div>
             </header>
-            <main class={""}>
-                <div class={"p-4 bg-shade-50"}>yososo</div>
-                <section>
-                    <Games/>
-                </section>
+            <main class={"flex flex-wrap justify-center gap-6 "}>
+                <button class="p">
+                    <img width="400" height="225" src="https://images.prismic.io/desplaines-rushstreetgaming/3ea58fe4-f291-4e9b-9750-2c6cbd38a2ab_04659_BOD-Photos_Poker-Room-01_1280x720_v1_220413.jpg?auto=compress,format" alt="" />
+                </button>
+                <button class="b">
+                    <img class={"w-[400] h-[225px]"} width="400" height="225" src="https://www.vismara.it/wp-content/uploads/2017/01/luxury_blackjack_table1.jpg" alt="" />
+                </button>
+                <button class="r">
+                    <img width="400" height="225" src="https://homburg1.de/wp-content/uploads/2022/03/Roulette-Casino-Gluecksspiel.jpg" alt="" />
+                </button>
             </main>
         </>
-    );
-}
-
-function Games() {
-    return [].map(Game);
-}
-
-function Game() {
-    return (
-        <div>Game</div>
     );
 }
