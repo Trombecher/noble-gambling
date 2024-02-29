6fb10ea301d7beb1c4974a22db904a130042b593
Tobias Hillemanns <Tobias.Hillemanns@schule.local>
2024-02-29 09:28:17 +0100

    impl roulette

--- src/games/roulette.tsx
+++ src/games/roulette.tsx
@@ -1,9 +1,22 @@
+import { Box } from "aena";
 import { Game } from "../App"
+import { insertBoxToString } from "aena/glue";
+
+enum Color {
+    Black = "Black",
+    Red = "Red"
+}
 
 export const Roulette: Game = ({balance}) => {
+    const result = new Box<Color | undefined>(undefined);
+
     return (
         <>
-            <div></div>
+            <div>Result {insertBoxToString(result, color => color || "")}</div>
+            <button></button>
+            <button onclick={() => {
+                result.value = Math.random() > .5 ? Color.Red : Color.Black
+            }}>Gamble</button>
         </>
     )
 }
