4408c9a0b1874cee909cd88d0fe612f92314a0f4
Niklas Post <Niklas.Post@schule.local>
2024-02-29 09:16:52 +0100

    adjust pictures

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,18 @@ export default function App() {
     return (
         <>
             <header class={"flex p-4 w-full"}>
-                <h1 class={"mr-auto text-shade-50"}>Noble Gambling</h1>
+                <h1 class={"mr-auto text-shade-50 text-3xl"}>Noble Gambling</h1>
                 <div>${insertBoxAsText(balance)}</div>
             </header>
             <main class={"flex flex-wrap justify-center gap-6 "}>
-                <button class="p">
-                    <img width="400" height="225" src="https://images.prismic.io/desplaines-rushstreetgaming/3ea58fe4-f291-4e9b-9750-2c6cbd38a2ab_04659_BOD-Photos_Poker-Room-01_1280x720_v1_220413.jpg?auto=compress,format" alt="" />
+                <button class="Slots">
+                    <img width="400" height="225" src="/Slots.jpg" alt="" />
                 </button>
-                <button class="b">
-                    <img class={"w-[400] h-[225px]"} width="400" height="225" src="https://www.vismara.it/wp-content/uploads/2017/01/luxury_blackjack_table1.jpg" alt="" />
+                <button class="Roulette">
+                    <img width="400" height="225" src="/Roulette.jpg" alt="" />
                 </button>
-                <button class="r">
-                    <img width="400" height="225" src="https://homburg1.de/wp-content/uploads/2022/03/Roulette-Casino-Gluecksspiel.jpg" alt="" />
+                <button class="Poker">
+                    <img  width="400" height="225" src="/Poker.jpg" alt="" />
                 </button>
             </main>
         </>
