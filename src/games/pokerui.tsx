1f1f627b15496331594a29373a785a1a22e2f26c
chris <christian-nolden@outlook.com>
2024-03-08 15:29:49 +0100

    Centred

--- src/games/pokerui.tsx
+++ src/games/pokerui.tsx
@@ -1,11 +1,13 @@
 import {Game} from "../games";
-import {PokerTable, HandType} from "./pokerframe";
+import {PokerTable} from "./pokerframe";
 import {insertBoxArray, insertBoxToString} from "aena/glue";
 import {JSX} from "aena";
 
 const SUIT_MAP = ["clubs", "hearts", "spades", "diamonds"]
 const RANK_MAP = ["error", "error", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                             "jack", "queen", "king", "ace"]
+const HAND_MAP = ["none", "High Card", "One Pair", "Two Pair", "Three of a Kind",
+                            "Straight", "Flush", "Full House", "Four of a Kind", "Straight Flush", "Royal Flush"]
 const LOCAL_PLAYER = 0
 
 export const Poker: Game = () => {
@@ -16,10 +18,11 @@ export const Poker: Game = () => {
     game.player[0]!.updateHand([])
     return (
         <>
-            {CommunityCards(game)}
-            {PlayerCards(game, LOCAL_PLAYER)}
-            <div class={"justify-center ml-auto"}>
-                <div>{insertBoxToString(game.player[LOCAL_PLAYER]!.hand.rank, (rank) => HandType[rank])}</div>
+            <div class={"justify center"}>
+                {CommunityCards(game)}
+                {PlayerCards(game, LOCAL_PLAYER)}
+            </div>
+            <div class={"justify-center m-auto"}>
                 <button onclick={() => {
                     game.reset();
                     game.distribute();
@@ -32,23 +35,29 @@ export const Poker: Game = () => {
     );
 }
 
-function PlayerCards(game: PokerTable, n: number): JSX.Element{
+function CommunityCards(game: PokerTable): JSX.Element{
     return (
-        <div class={`flex flex-wrap gap-6 ml-auto p-6 justify-center`}>
-            {insertBoxArray(game.player[n]!.card, (card) => (
+        <div class={`flex gap-6 p-6 pb-16 justify-center`}>
+            {insertBoxArray(game.mid, (card) => (
                 <img src={`/noble-gambling/Cards/${RANK_MAP[card.rank]}_of_${SUIT_MAP[card.suit]}.png`} alt=""
                      width="75" style="wrap" class={""}/>
             ))}
         </div>
     )
 }
-function CommunityCards(game: PokerTable): JSX.Element{
+
+function PlayerCards(game: PokerTable, n: number): JSX.Element{
     return (
-        <div class={`flex flex-wrap gap-6 p-6 pb-16 justify-center`}>
-            {insertBoxArray(game.mid, (card) => (
-                <img src={`/noble-gambling/Cards/${RANK_MAP[card.rank]}_of_${SUIT_MAP[card.suit]}.png`} alt=""
-                     width="75" style="wrap" class={""}/>
-            ))}
-        </div>
+        <>
+            <div class={`ml-auto mr- p-6`}>
+                <div class={"justify-center flex gap-6 "}>
+                    {insertBoxArray(game.player[n]!.card, (card) => (
+                        <img src={`/noble-gambling/Cards/${RANK_MAP[card.rank]}_of_${SUIT_MAP[card.suit]}.png`} alt=""
+                             width="75" style="wrap" class={""}/>
+                    ))}
+                </div>
+                <div class={"text-center pt-4"}>{insertBoxToString(game.player[LOCAL_PLAYER]!.hand.rank, (rank) => HAND_MAP[rank]!)}</div>
+            </div>
+        </>
     )
 }
